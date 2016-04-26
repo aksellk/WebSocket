a1f9c3344ad1016babd21b51a840af805ec9a8cd
Aksel Karlsen <aksellk@student.hist.no>
2016-04-26 14:36:41 +0200

    javadoc Handler

--- NettverksprogrammeringProsjekt/src/message/Handler.java
+++ NettverksprogrammeringProsjekt/src/message/Handler.java
@@ -5,12 +5,13 @@ import java.net.Socket;
 import server.Main;
 
 /**
- *
+ * Handles communication with the client
+ * 
  * @author Aksel
  */
 public class Handler {
     
-    private long id;
+    private long id; // thread_id
     private MessageHandler handler;
     private Main main;
     
@@ -32,6 +33,18 @@ public class Handler {
         return id;
     }
     
+    /**
+     * Handles communication with the client:
+     * 
+     * If a close-frame is recieved from the client; the connection closes 
+     * and the thread is taken out of the active threads
+     * 
+     * If a message is recieved from the client, a response-frame is produced and gets sent to all clients
+     * the message field is then set to null 
+     * 
+     * @param connection The socket which contains the InputStream with the messages from the client
+     * @throws Exception 
+     */
     public void handle(Socket connection) throws Exception {    
         
         try(InputStream is = connection.getInputStream()) {  
@@ -54,6 +67,10 @@ public class Handler {
         }
     }
     
+    /**
+     * Creates a close-message which for the client when the server takes initiative to disconnect
+     * @return the close-frame
+     */
     public byte[] close() {
          MessageHandler handler = new MessageHandler();
          return handler.getM().createCloseMessage();
